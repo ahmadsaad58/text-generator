cce041797104756e9862d6a7bd86bd26b6b3304a
Saad Ahmad <ahmadsaad58@gmail.com>
2021-11-08 00:58:38 -0600

    adding utils.py

--- Model/utils.py
+++ Model/utils.py
@@ -1,7 +1,7 @@
-from torch.utils.data import Dataset
-import torch
 from collections import Counter
 import pandas as pd
+import torch
+from torch.utils.data import Dataset
 
 PATH = "data/data.csv"
 
